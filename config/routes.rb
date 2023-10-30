877c1b904492c44ae2f84f69dba9013758d3d761
EC2 Default User <ec2-user@ip-172-31-46-53.ap-northeast-1.compute.internal>
2023-10-30 09:02:48 +0000

    ルーティング作成開始前

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :favorites
+  devise_for :comments
+  devise_for :posts
+  devise_for :users
+  devise_for :admins
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
