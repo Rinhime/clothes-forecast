b252bdf8a417a0ba0f9664aa4a09364deb7ec4ea
EC2 Default User <ec2-user@ip-172-31-46-53.ap-northeast-1.compute.internal>
2023-11-02 04:49:28 +0000

    ユーザー-新規登録エラー解消

--- db/migrate/20231030081934_devise_create_users.rb
+++ db/migrate/20231030081934_devise_create_users.rb
@@ -7,7 +7,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.1]
       t.string :email, null: false
       t.string :encrypted_password, null: false
       t.string :name, null: false
-      t.boolean :is_deleted, null: false
+      t.boolean :is_deleted, default: false, null: false
       
       ## Recoverable
       t.string   :reset_password_token
--- db/schema.rb
+++ db/schema.rb
@@ -67,7 +67,7 @@ ActiveRecord::Schema.define(version: 2023_10_30_082007) do
     t.string "email", null: false
     t.string "encrypted_password", null: false
     t.string "name", null: false
-    t.boolean "is_deleted", null: false
+    t.boolean "is_deleted", default: false, null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
